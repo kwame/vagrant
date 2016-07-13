da1783bf5152703b2de147984ac0cda7af99ecf8
Kwame Bahena <daniel.bahena@gmail.com>
2016-07-13 16:12:09 -0500

    Adding comments and download url for puppetserver config

--- scripts/centos7-puppetmaster.sh
+++ scripts/centos7-puppetmaster.sh
@@ -2,8 +2,18 @@
 
 # Updating and install CentOS packages
 
-# Update 
+# Update system
 yum -y update --exclude=kernel
+
+# Installing puppetserver 
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-yum -y install puppetserver
+yum -y install puppetserver ntp
+
+# Downloading puppetserver config file 
+wget -q https://raw.githubusercontent.com/kwame/vagrant/master/files/puppetserver -O /etc/sysconfig/puppetserver
+
+# Starting services
 sudo systemctl start puppetserver.service
+systemctl start ntpd.service
+systemctl enable ntpd.service
+
