d8bce51e667a1e1970ec37e4483ee5297078f933
Kwame Bahena <daniel.bahena@gmail.com>
2016-07-13 16:24:59 -0500

    Adding services and fixing hostname

--- scripts/centos7-puppetmaster.sh
+++ scripts/centos7-puppetmaster.sh
@@ -5,6 +5,9 @@
 # Update system
 yum -y update --exclude=kernel
 
+# Setting hostname
+hostnamectl set-hostname puppet --static
+
 # Installing puppetserver 
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 yum -y install puppetserver ntp
@@ -14,5 +17,6 @@ wget -q https://raw.githubusercontent.com/kwame/vagrant/master/files/puppetserve
 
 # Starting services
 systemctl start puppetserver.service
-systemctl start ntpd.service
+systemctl restart ntpd.service
 systemctl enable ntpd.service
+systemctl enable puppetserver.service
